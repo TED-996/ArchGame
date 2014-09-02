5eba476027be6575194214d8f01751c7335159f9
Eduard Budaca <budaca.eduard@gmail.com>
2014-09-02 14:07:17 +0300

    Added obstructions and ContentToIArchLoadable

---
+++ Source/Components/IArchObstruction.cs
@@ -0,0 +1,15 @@
+﻿using ArchGame.Input;
+
+namespace ArchGame.Components {
+	/// <summary>
+	/// IArchObstruction is an interface for components that obstruct certain parts of the screen, so that components drawn below it
+	/// will not receive mouse clicks made on this component.
+	/// Requires IZIndexComponent to get the ZIndex of the obstruction.
+	/// </summary>
+	public interface IArchObstruction : IZIndexComponent {
+		/// <summary>
+		/// Get the rectangles that should be obstructed.
+		/// </summary>
+		void ObstructArea(InputManager inputManager);
+	}
+}
---
+++ Source/Components/IZIndexComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace ArchGame.Components {
+	/// <summary>
+	/// IZIndexComponent is an interface for components that follow an order in which they are drawn on the screen.
+	/// </summary>
+	public interface IZIndexComponent {
+		/// <summary>
+		/// The ZIndex is a number that represents the order in which components should be drawn on the screen.
+		/// A component with a lower ZIndex will be drawn before, and therefore behind components with a higher ZIndex.
+		/// </summary>
+		int ZIndex { get; }
+	}
+}
