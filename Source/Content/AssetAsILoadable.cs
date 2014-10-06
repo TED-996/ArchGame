b3be824a9b9995187e58b04d7ba6678b5cef3b51
Eduard Budaca <budaca.eduard@gmail.com>
2014-10-06 16:52:35 +0300

    Added AssetAsILoadable classes.

---
+++ Source/Content/AssetAsILoadable.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using ArchGame.Components;
+using Microsoft.Xna.Framework.Content;
+using System.Linq;
+
+namespace ArchGame.Content {
+	/// <summary>
+	/// Wraps a type and a filename into an IArchLoadable.
+	/// Use it to add content to the LoadableSet.
+	/// You may also use it as a component in the ComponentList.
+	/// </summary>
+	/// <typeparam name="T">The type of the asset</typeparam>
+	public class AssetAsILoadable<T> : IArchLoadable {
+		readonly string filename;
+
+		/// <summary>
+		/// Get the asset that was loaded.
+		/// </summary>
+		public T Asset { get; private set; }
+
+		/// <summary>
+		/// Initialize a new object of type AssetAsIArchLoadable
+		/// </summary>
+		/// <param name="newFilename">The filename of the asset</param>
+		public AssetAsILoadable(string newFilename) {
+			filename = newFilename;
+		}
+
+		/// <summary>
+		/// Load the asset.
+		/// </summary>
+		/// <param name="contentManager">The ContentManager to load the asset from</param>
+		public void LoadContent(ContentManager contentManager) {
+			Asset = contentManager.Load<T>(filename);
+		}
+	}
+
+	/// <summary>
+	/// Wraps a type and multiple filenames into an IArchLoadable.
+	/// Use it to add content to the LoadableSet or to load assets in bulk.
+	/// </summary>
+	/// <typeparam name="T">The type of the assets</typeparam>
+	public class MultipleAssetsAsILoadable<T> : IArchLoadable {
+		readonly string[] filenames;
+
+		/// <summary>
+		/// Get the assets that were loaded.
+		/// </summary>
+		public List<T> Assets { get; private set; }
+
+		/// <summary>
+		/// Initialize a new object of type MultipleAssetsAsIArchLoadable
+		/// </summary>
+		/// <param name="newFilenames">The filenames of the assets</param>
+		public MultipleAssetsAsILoadable(params string[] newFilenames) {
+			filenames = newFilenames;
+			Assets = null;
+		}
+
+		/// <summary>
+		/// Load the assets.
+		/// </summary>
+		/// <param name="contentManager">The ContentManager to load the assets from</param>
+		public void LoadContent(ContentManager contentManager) {
+			Assets = filenames.Select(contentManager.Load<T>).ToList();
+		}
+	}
+}
